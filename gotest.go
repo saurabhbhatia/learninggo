2d5ee9de1f5de22bbe4007e261aefe0ca67c028a
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2016-05-30 09:55:03 +1000

    variables

---
+++ gotest.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var product int = 3 * 5
+	fmt.Println("The product is", product)
+
+	var message string = "this is a test message"
+	fmt.Println("All of us know that", message)
+}
