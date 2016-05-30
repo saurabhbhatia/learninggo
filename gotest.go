ba89cd5f300e9e8197342c39f6678f7c4e2122cc
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2016-05-30 21:21:33 +1000

    fix tabs

--- gotest.go
+++ gotest.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var product int = 3 * 5
-	fmt.Println("The product is", product)
+  var product int = 3 * 5
+  fmt.Println("The product is", product)
 
-	var message string = "this is a test message"
-	fmt.Println("All of us know that", message)
+  var message string = "this is a test message"
+  fmt.Println("All of us know that", message)
 }
