78f96924d28b1c4b93a28746dd8e7c8bb77d85aa
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2016-05-30 21:20:18 +1000

    fix tabs

--- assignmentCheck.go
+++ assignmentCheck.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
   var assignmentCheck string
-	assignmentCheck = "first value"
-	fmt.Println(assignmentCheck)
+  assignmentCheck = "first value"
+  fmt.Println(assignmentCheck)
   assignmentCheck += " second value"
   fmt.Println(assignmentCheck)
 }
