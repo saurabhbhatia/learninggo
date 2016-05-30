eb435283a182fdce8ae680905906d2a0a8c948f1
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2016-05-30 20:17:05 +1000

    multiple variables under one var statement

---
+++ multiVariable.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  var (
+    message = "hello world"
+    randomNumber = 3456
+    aDecimalNumber = 43.25
+    aBooleanVariable = true
+  )
+
+  fmt.Println(message)
+  fmt.Println(randomNumber)
+  fmt.Println(aDecimalNumber)
+  fmt.Println(aBooleanVariable)
+}
