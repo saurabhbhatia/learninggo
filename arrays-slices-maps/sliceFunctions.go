474046e2f34fc0711090f0c253c0bcfcf4358a03
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2016-06-01 16:51:21 +1000

    Slice functions

---
+++ arrays-slices-maps/sliceFunctions.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  // append slices
+  sliceX := []int{ 1, 2, 3 }
+  sliceY := append(sliceX, 4, 5)
+  fmt.Println(sliceX, sliceY)
+
+  sliceA := []int{1, 2, 3}
+  sliceB := make([]int, 2)
+  copy(sliceA, sliceB)
+  fmt.Println(sliceA, sliceB)
+}
