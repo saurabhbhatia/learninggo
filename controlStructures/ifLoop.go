2b02ead59a1a02f1de66622edee29009fa8107ad
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2016-06-01 11:20:41 +1000

    if else if loop

--- controlStructures/ifLoop.go
+++ controlStructures/ifLoop.go
@@ -10,4 +10,14 @@ func main() {
       fmt.Println(i," is odd")
     }
   }
+
+  for i := 1; i <= 20; i++ {
+    if i % 2 == 0 {
+      fmt.Println(i, " is divisible by 2")
+    } else if i % 3 == 0 {
+      fmt.Println(i, " is divisible by 3")
+    } else if i % 4 == 0 {
+      fmt.Println(i, " is divisible by 4")
+    }
+  }
 }
