2be6d779d612f146e0a49d3a3ecefa048f46c161
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2016-06-05 20:13:13 +1000

    more pointer exercices

--- golang-book/pointers/problemsChapter8.go
+++ golang-book/pointers/problemsChapter8.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// How do you get the memory address of a variable?
+// ans : by adding an astrix and printing it out - x *int points towards the memory location of an integer with value x
+
+//how do you assing a value to the pointer
+// *x = newValue
+// access this value using &x
+
+// how do you create a new pointer
+// x := new(int)
+
 func square(x *float64) {
   *x = *x * *x
 }
@@ -15,9 +25,13 @@ func swapInt(a *int, b *int) {
 }
 
 func main() {
+
+  // finding square of a number
   x := 1.5
   square(&x)
   fmt.Println(x)
+
+  // swapping integers
   a := 4
   b := 5
   swapInt(&a, &b)
