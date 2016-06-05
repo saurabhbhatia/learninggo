4f06c627c63446e555a4cbd0966313e6e53faf0d
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2016-06-05 16:18:47 +1000

    use of panic

--- golang-book/functions/functionCheck.go
+++ golang-book/functions/functionCheck.go
@@ -10,9 +10,13 @@ func main() {
 
 // custom function
 func average(xs []float64) float64 {
-  total := 0.0
-  for _, v := range xs {
-    total += v
+  if len(xs) == 0 {
+    panic("this array is blank")
+  } else {
+    total := 0.0
+    for _, v := range xs {
+      total += v
+    }
+    return (total/float64(len(xs)))
   }
-  return (total/float64(len(xs)))
 }
